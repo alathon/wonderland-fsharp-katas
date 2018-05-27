55478dbc8924a4a55d8d77735954a5f0ce100aeb
Martin Grünbaum <martin@itsolveonline.net>
2018-05-27 19:21:47 +0200

    Add wonderland-number solution

--- wonderland-fsharp-katas/wonderland-number.fsx
+++ wonderland-fsharp-katas/wonderland-number.fsx
@@ -1,16 +1,32 @@
 // See the file wonderland-number.md for detailed information.
-
-let wonderlandNumber () = 42
-
 let haveSameDigits (n1:int,n2:int) =
     (string n1 |> Set.ofSeq) = (string n2 |> Set.ofSeq)
+
+// Curried version of haveSameDigits for partial application
+let haveSameDigits' n1 n2 = haveSameDigits(n1,n2)
+
+// For some given int i, first create array of i multiplied with mults,
+// then filter out the ones that don't have the same digits as i. If
+// all entries are still there, we're good.
+let checkMultipliedWith (mults:int list) (i:int) = 
+    List.map ((*) i) mults |>
+    List.filter (haveSameDigits' i) |> 
+    List.length = mults.Length
+
+// Given a sequence of possible ints, try and find the first one to 
+// pass wonderland conditions.
+// We fall back to 0 in case there isn't one. Opts to default to 0 instead
+// of returning an Option and needing to modify the code in tests().
+let wonderlandNumber () = seq { 100000 .. (int (1000000/6)) } |>  
+                          Seq.tryFind (checkMultipliedWith [2 .. 6]) |>
+                          Option.defaultWith (fun _ -> 0)
 
 #r @"../packages/Unquote/lib/net45/Unquote.dll"
 open Swensen.Unquote
 
 let tests () =
 
-    let wonderNum = wonderlandNumber ()
+    let wonderNum = wonderlandNumber()
 
     test <@ (string wonderNum).Length = 6 @>
 
--- wonderland-fsharp-katas/wonderland-number.md
+++ wonderland-fsharp-katas/wonderland-number.md
@@ -34,6 +34,7 @@ If you haven't solved your kata yet - Don't Peek!
 * https://github.com/epeicher/wonderland-fsharp-katas/blob/master/wonderland-fsharp-katas/wonderland-number.fsx
 * https://github.com/htoma/wonderland-fsharp-katas/blob/master/wonderland-fsharp-katas/wonderland-number.fsx
 * https://github.com/nestordemeure/wonderland-fsharp-katas/blob/master/wonderland-fsharp-katas/wonderland-number.fsx
+* https://github.com/alathon/wonderland-fsharp-katas/blob/master/wonderland-fsharp-katas/wonderland-number.fsx
 
 ## License
 
